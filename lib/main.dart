46dfadad06d19e6f3eea35de08172e2c16b97b3d
codewithx4 <113529442+codewithx4@users.noreply.github.com>
2023-07-19 12:07:18 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,17 @@
-import "package:flutter/material.dart";
-import "package:flutter_local_notifications/flutter_local_notifications.dart";
-import "package:timezone/data/latest.dart";
-import "package:timezone/timezone.dart";
+import 'package:flutter/material.dart';
 
+import 'Screens/HomePage.dart';
 
 void main() {
-  runApp(AlarmApp());
+  runApp(MyApp());
 }
 
-class AlarmApp extends StatelessWidget {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-  AlarmApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Sanmay's Alarm App",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: AlarmScreen(flutterLocalNotificationsPlugin),
-    );
-  }
-}
-
-class AlarmScreen extends StatefulWidget {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-
-  const AlarmScreen(this.flutterLocalNotificationsPlugin, {super.key});
-
-  @override
-  State<AlarmScreen> createState() => _AlarmScreenState();
-}
-
-class _AlarmScreenState extends State<AlarmScreen> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  TimeOfDay _selectedTime = TimeOfDay.now();
-
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime,
-    );
-
-    if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
-      });
-    }
-  }
-
-  Future<void> _scheduleAlarm() async {
-    final now = TZDateTime.now(local);
-    final selectedDateTime = TZDateTime(
-      local,
-      now.year,
-      now.month,
-      now.day,
-      _selectedTime.hour,
-      _selectedTime.minute,
-    );
-
-    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      "Alarm App Notification Channel",
-      "Something",
-      "Description",
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-    const platformChannelSpecifics = NotificationDetails(
-      android: androidPlatformChannelSpecifics,
-    );
-
-    await widget.flutterLocalNotificationsPlugin.zonedSchedule(
-      0,
-      "Alarm",
-      "Wake up Bhai !",
-      selectedDateTime,
-      platformChannelSpecifics,
-      androidAllowWhileIdle: true,
-      matchDateTimeComponents: DateTimeComponents.time,
-      uiLocalNotificationDateInterpretation:
-      UILocalNotificationDateInterpretation.absoluteTime,
-    );
-
-    if (context.mounted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Alarm set for ${selectedDateTime.toString()} Bro"),
-        ),
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    initializeTimeZones();
-    const initializationSettingsAndroid =
-    AndroidInitializationSettings("@mipmap/ic_launcher");
-    const initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid,
-      iOS: null,
-    );
-
-    widget.flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: const Text("Sanmay's Alarm App"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Selected Time: ${_selectedTime.format(context)}",
-              style: const TextStyle(fontSize: 20),
-            ),
-            ElevatedButton(
-              onPressed: () => _selectTime(context),
-              child: const Text("Select Time"),
-            ),
-            ElevatedButton(
-              onPressed: () => _scheduleAlarm(),
-              child: const Text("Set Alarm"),
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
